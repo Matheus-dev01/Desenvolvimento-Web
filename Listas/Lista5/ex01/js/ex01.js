389f64e20dc6f10c4af0f2af514b20d9b34796c5
Matheus-dev01 <matheuspt169@gmail.com>
2024-05-22 19:29:41 -0300

    Lista 5 - Primeiros passos com JS

---
+++ Listas/Lista5/ex01/js/ex01.js
@@ -0,0 +1,6 @@
+var nome = prompt('Digite seu nome: ')
+var idade = prompt('Digite sua idade: ')
+var empregoatual = prompt('Digite seu emprego atual: ')
+var endereco = prompt('Digite seu endereço: ')
+
+alert(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é: ${empregoatual} \nE mora no endereço: ${endereco}`)
---
+++ Listas/Lista5/ex02/js/ex02.js
@@ -0,0 +1,6 @@
+var nome = "Matheus"
+var idade = 19
+var empregoatual = "Desenvolvedor Web"
+var endereco = "Rua sem fim"
+
+console.log(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é: ${empregoatual} \nE mora no endereço: ${endereco}`)
