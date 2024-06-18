6de2ffd7f18933f3f8e045a3b58703fc4ba64caf
Matheus-dev01 <matheuspt169@gmail.com>
2024-06-18 19:42:05 -0300

    Lista 6 - Arrays e Objetos em Javascript

---
+++ Listas/Lista6/js/ex01.js
@@ -0,0 +1,32 @@
+var PIX = {
+    chavePix: '1A2B3C',
+    valorTranferencia: 299.99,
+    dataTransacao: '18 de Junho',
+    descricao: 'Pagar conta de Luz',
+}
+
+var pagador = {
+    nome: 'Alex',
+    documento: '465.131.031-15'
+}
+
+var recebedor = {
+    nome: 'EDP Brasil',
+    documento: '64.805.016/0001-02'
+}
+
+
+var historicoDeTransicoes = {
+    transicoes: [
+        {
+            data: '6 de junho',
+            valor: 29.99,
+            descricao: 'Compra de brinquedo'
+        },
+        {
+            data: '25 de Maio',
+            valor: 299.99,
+            descricao: 'Compra de Microondas'
+        }
+    ]
+}
